8fe7009526a4ebeaab60641310355816f14fcfb5
BedirhanMaden <bmaden20@ku.edu.tr>
2023-11-21 19:23:48 +0300

    Implemented the draft of the classes under potion and player

--- domain/cards/IngredientCard.java
+++ domain/cards/IngredientCard.java
@@ -1,4 +1,16 @@
 package domain.cards;
 
+import domain.potion.Molecule;
+
 public class IngredientCard extends Card{
+	
+	int ingredientId;
+	String name;
+	int pointValue;
+	Molecule molecule;
+	
+	public Molecule createMolecule(int moleculeId) {
+		return new Molecule(moleculeId);
+	}
+	
 }
--- domain/potion/Atom.java
+++ domain/potion/Atom.java
@@ -1,4 +1,46 @@
 package domain.potion;
 
 public class Atom {
+	int atomSize; //0 small, 1 big
+	int atomColor; // 0 red, 1 green, 2 blue
+	int atomSign; //0 negative, 1 positive
+	
+	public Atom(int atomSize, int atomColor, int atomSign) {
+		super();
+		this.atomSize = atomSize;
+		this.atomColor = atomColor;
+		this.atomSign = atomSign;
+	}
+
+	public int getAtomSize() {
+		return atomSize;
+	}
+
+	public void setAtomSize(int atomSize) {
+		this.atomSize = atomSize;
+	}
+
+	public int getAtomColor() {
+		return atomColor;
+	}
+
+	public void setAtomColor(int atomColor) {
+		this.atomColor = atomColor;
+	}
+
+	public int getAtomSign() {
+		return atomSign;
+	}
+
+	public void setAtomSign(int atomSign) {
+		this.atomSign = atomSign;
+	}
+
+	
+	
+	
+
+	
+	
+	
 }
