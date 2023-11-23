14e162c1cfbf2ee71b624d1f560bf6947fed8841
BedirhanMaden <bmaden20@ku.edu.tr>
2023-11-23 23:41:03 +0300

    Implemented Player related classes and Game class further

--- domain/player/Player.java
+++ domain/player/Player.java
@@ -11,23 +11,68 @@ public class Player {
 	
 	int playerId;
 	String playerName;
-	int playerHealth = 3;
+	String playerPassword;
+	
+	
 	PlayerToken playerToken;
 	
 	
 	
-	public Player(int playerId, String playerName) {
+	
+	public Player(int playerId, String playerPassword, String playerName) {
 	
 		this.playerId = playerId;
 		this.playerName = playerName;
-		this.playerHealth = playerHealth;
+		this.playerPassword=playerPassword;
+		
 		
 	}
 	
 	public void setPlayerToken(PlayerToken playerToken) {
 		this.playerToken=playerToken;
 	}
+	
+	public PlayerInventory getInventory() {
+		return this.playerToken.playerInventory;
+	}
+	
+	public void addHealth() {
+		this.playerToken.addHealth();
+	}
+	
+	public void reduceHealth() {
+		this.playerToken.reduceHealth();
+	}
 
+	public int getPlayerId() {
+		return playerId;
+	}
+
+	public void setPlayerId(int playerId) {
+		this.playerId = playerId;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public String getPlayerPassword() {
+		return playerPassword;
+	}
+
+	public void setPlayerPassword(String playerPassword) {
+		this.playerPassword = playerPassword;
+	}
+
+	public PlayerToken getPlayerToken() {
+		return playerToken;
+	}
+	
+	
 //
 //	public void transmuteIngredient(IngredientCard ingredient) {
 //		
