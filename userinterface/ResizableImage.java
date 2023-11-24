d378b8c7631fc06c10325c6dba2548f50ab10a52
batuogkoc <bgunduz21@ku.edu.tr>
2023-11-24 11:08:32 +0300

    added the resizableimage class

---
+++ userinterface/ResizableImage.java
@@ -0,0 +1,36 @@
+package userinterface;
+
+import java.awt.Image;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+/**
+ * a resizable image object that scales an image into the space the object occupies while preserving the aspect ration of the image
+ * @author batu
+ *
+ */
+public class ResizableImage extends JLabel{
+	/**
+	 * generate the panel that holds the dynamically resizable image
+	 * @param image image to be displayed
+	 */
+	public ResizableImage(ImageIcon image) {
+		float imageAspectRatio = (float)image.getIconWidth()/(float)image.getIconHeight(); //aspect ratio of original image
+		setHorizontalAlignment(JLabel.CENTER);
+		//whenever the object is resized, redraw the image
+		addComponentListener(new ComponentAdapter() {
+			public void componentResized(ComponentEvent e) {
+				float aspectRatio = getWidth()/getHeight(); //get aspect ratio of the object
+				if(imageAspectRatio>aspectRatio) {
+					//Scale image to fit it horizontally (add empty space to the top and bottom of image)
+					setIcon(new ImageIcon(image.getImage().getScaledInstance((int)getWidth(), (int)(getWidth()/imageAspectRatio), Image.SCALE_DEFAULT)));
+				}
+				else {
+					//Scale image to fit it vertically (add empty space to the sides of the image)
+					setIcon(new ImageIcon(image.getImage().getScaledInstance((int)(getHeight()*imageAspectRatio), (int)(getHeight()), Image.SCALE_DEFAULT)));
+				}
+			}
+		});
+	}	
+}
