686e7dd5bc9078ec1ba93d88100c7e8785130b03
larakucukdevlet <lkucukdevlet20@ku.edu.tr>
2023-11-29 14:59:59 +0300

    Implementation of Theory package

--- domain/theory/AlchemyMarkers.java
+++ domain/theory/AlchemyMarkers.java
@@ -1,4 +1,29 @@
 package domain.theory;
 
+import domain.cards.IngredientCard;
+
 public class AlchemyMarkers {
+	private IngredientCard ingredient;
+    private Hypotheses hypothesis;
+	public AlchemyMarkers() {
+		super();
+	}
+	public AlchemyMarkers(IngredientCard ingredient, Hypotheses hypothesis) {
+		super();
+		this.ingredient = ingredient;
+		this.hypothesis = hypothesis;
+	}
+	public IngredientCard getIngredient() {
+		return ingredient;
+	}
+	public void setIngredient(IngredientCard ingredient) {
+		this.ingredient = ingredient;
+	}
+	public Hypotheses getHypothesis() {
+		return hypothesis;
+	}
+	public void setHypothesis(Hypotheses hypothesis) {
+		this.hypothesis = hypothesis;
+	}
+    
 }
--- domain/theory/Hypotheses.java
+++ domain/theory/Hypotheses.java
@@ -1,4 +1,30 @@
 package domain.theory;
 
+import domain.cards.IngredientCard;
+import domain.potion.Molecule;
+
 public class Hypotheses {
+	private IngredientCard ingredient;
+	private Molecule molecule;
+	public Hypotheses() {
+		super();
+	}
+	public Hypotheses(IngredientCard ingredient, Molecule molecule) {
+		super();
+		this.ingredient = ingredient;
+		this.molecule = molecule;
+	}
+	public IngredientCard getIngredient() {
+		return ingredient;
+	}
+	public void setIngredient(IngredientCard ingredient) {
+		this.ingredient = ingredient;
+	}
+	public Molecule getMolecule() {
+		return molecule;
+	}
+	public void setMolecule(Molecule molecule) {
+		this.molecule = molecule;
+	}
+	
 }
