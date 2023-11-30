52e3c9a3e8d27461ad74d5a1d5d1287a6f20b104
larakucukdevlet <lkucukdevlet20@ku.edu.tr>
2023-11-30 11:11:13 +0300

    UI Brew Portion Area initialized

--- userinterface/Main.java
+++ userinterface/Main.java
@@ -1,5 +1,11 @@
 package userinterface;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import techServices.UserInfoSaver;
+import userinterface.util.GlobalDimensions;
 
 public class Main {
 	/**
@@ -10,6 +16,23 @@ public class Main {
 
 		new LoginSignUpWindow();
 		//UserInfoSaver.addToTxtFile("Mina","79776", "userinfo.txt");
+		JPanel brewpot=new BrewPotionPanel();
+		JFrame brewpotFrame = new JFrame();
+		brewpotFrame.setUndecorated(true);
+		brewpotFrame.setMaximumSize(new Dimension(1920, 1080));
+		brewpotFrame.setBounds(0, 0, 1920, 1080);
+		//LoginSignUpWindowFrame.setExtendedState(JFrame.NORMAL);
+
+	    brewpotFrame.setTitle("Welcome to Alchemists!");
+		brewpotFrame.setResizable(false);
+	    brewpotFrame.setSize(GlobalDimensions.FULL_SCREEN);
+		brewpotFrame.setPreferredSize(GlobalDimensions.FULL_SCREEN);
+		brewpotFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+
+
+		brewpotFrame.add(brewpot);
+		brewpotFrame.setVisible(true);
 
 
 	}
