348b8d8a67c0b8569c4502ee36b48d17c50b8b77
batuogkoc <bgunduz21@ku.edu.tr>
2023-12-02 15:08:20 +0300

    added action check and usererrorexception use

--- domain/boards/PublicationBoard.java
+++ domain/boards/PublicationBoard.java
@@ -1,5 +1,6 @@
 package domain.boards;
 import java.util.*;
+import exception.*;
 
 import domain.theory.*;
 import domain.player.*;
@@ -14,23 +15,26 @@ public class PublicationBoard extends Board{
         this.hypotheses = new ArrayList<Hypotheses>();
     }
     
-    public void publishTheory(Player player, IngredientCard ingredient, Molecule hypothesizedMolecule) throws Exception{
+    public void publishTheory(Player player, IngredientCard ingredient, Molecule hypothesizedMolecule) throws UserErrorException{
     	PlayerInventory inv = player.getInventory();
     	int gold = player.getPlayerToken().getGold();
+    	if(!player.getPlayerToken().hasActionsLeft()) {
+    		throw new UserErrorException("The user has no more actions left!");
+    	}
     	if(!inv.getPlayerIngredientCardList().contains(ingredient)) {
-    		throw new Exception("The user does not own this ingredient!");
+    		throw new UserErrorException("The user does not own this ingredient!");
     	}
     	if(gold < 1) {
-    		throw new Exception("The user does not have enough gold!");
+    		throw new UserErrorException("The user does not have enough gold!");
     	}
     	if(hypotheses.stream().map(t -> t.getIngredient()).anyMatch(t -> t.equals(ingredient))){
-    		throw new Exception("This ingredient already has a theory on it!");
+    		throw new UserErrorException("This ingredient already has a theory on it!");
     	}
     	Hypotheses hypothesis = new Hypotheses(player, ingredient, hypothesizedMolecule);
     	player.getInventory().addHypoteses(hypothesis);
     	hypotheses.add(hypothesis);
-    	player.getPlayerToken().setReputation(player.getPlayerToken().getReputation()+1);
-    	player.getPlayerToken().setGold(gold - 1);
+    	player.getPlayerToken().addReputationPoint(1);
+    	player.getPlayerToken().subtractGold(1);
     }
     
     public ArrayList<Hypotheses> getHypotheses() {
