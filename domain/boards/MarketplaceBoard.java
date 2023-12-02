14061243095966c5f0d85cbaf3d2ceadea23eb9a
batuogkoc <bgunduz21@ku.edu.tr>
2023-12-02 14:50:43 +0300

    added buy artifact functionality. Action reduction is missing

---
+++ domain/boards/MarketplaceBoard.java
@@ -0,0 +1,26 @@
+package domain.boards;
+
+import domain.cards.*;
+import domain.player.Player;
+import exception.UserErrorException;
+public class MarketplaceBoard extends Board {
+
+	public MarketplaceBoard(int size, String name) {
+		super(size, name);
+	}
+	
+	public void buyArtifact(Player player) throws UserErrorException{
+		if(player.getPlayerToken().getPlayerAction()<1) {
+			throw new UserErrorException("The user does not have any actions left!");
+		}
+		if(player.getPlayerToken().getGold() < 3) {
+			throw new UserErrorException("The user has insufficient gold!");
+		}
+		ArtifactCard artifactCard = ArtifactDeck.getInstance().removeArtifact();
+		player.getPlayerToken().addArtifactCard(artifactCard);
+		player.getPlayerToken().subtractGold(3);
+		//TODO: Apply artifact effects
+		//artifactCard.apply();
+	}
+
+}
