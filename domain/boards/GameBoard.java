1a730b21c6b0e4ade245871fbc9caea2b0c2e20c
batuogkoc <bgunduz21@ku.edu.tr>
2023-12-02 14:50:43 +0300

    added buy artifact functionality. Action reduction is missing

--- domain/boards/GameBoard.java
+++ domain/boards/GameBoard.java
@@ -1,58 +1,55 @@
 package domain.boards;
 
 public class GameBoard extends Board {
-    private Board deductionBoard;
-    private Board ingredientBoard;
-    private Board publicationBoard;
-    private Board potionBrewingBoard;
-
-    public GameBoard(int size, String name) {
-        super(size, name);
-    }
+    private final Board deductionBoard;
+    private final Board ingredientBoard;
+    private final Board publicationBoard;
+    private final Board potionBrewingBoard;
+    private final Board marketplaceBoard;
 
     public GameBoard(int size, String name, DeductionBoard deductionBoard, IngredientBoard ingredientBoard,
-                     PublicationBoard publicationBoard, PotionBrewingBoard potionBrewingBoard) {
+                     PublicationBoard publicationBoard, PotionBrewingBoard potionBrewingBoard, MarketplaceBoard marketplaceBoard) {
         super(size, name);
         this.deductionBoard = deductionBoard;
         this.ingredientBoard = ingredientBoard;
         this.publicationBoard = publicationBoard;
         this.potionBrewingBoard = potionBrewingBoard;
-    }
-
-    public GameBoard() {
-        super();
+        this.marketplaceBoard = marketplaceBoard;
     }
 
 	public Board getDeductionBoard() {
 		return deductionBoard;
 	}
 
-	public void setDeductionBoard(Board deductionBoard) {
-		this.deductionBoard = deductionBoard;
-	}
-
 	public Board getIngredientBoard() {
 		return ingredientBoard;
-	}
-
-	public void setIngredientBoard(Board ingredientBoard) {
-		this.ingredientBoard = ingredientBoard;
 	}
 
 	public Board getPublicationBoard() {
 		return publicationBoard;
 	}
 
-	public void setPublicationBoard(Board publicationBoard) {
-		this.publicationBoard = publicationBoard;
-	}
-
 	public Board getPotionBrewingBoard() {
 		return potionBrewingBoard;
 	}
-
-	public void setPotionBrewingBoard(Board potionBrewingBoard) {
-		this.potionBrewingBoard = potionBrewingBoard;
+	
+	public Board getMarketplaceBoard() {
+		return marketplaceBoard;
 	}
-    
+
+//	public void setPotionBrewingBoard(Board potionBrewingBoard) {
+//		this.potionBrewingBoard = potionBrewingBoard;
+//	}
+//    
+//	public void setPublicationBoard(Board publicationBoard) {
+//		this.publicationBoard = publicationBoard;
+//	}
+//	
+//	public void setIngredientBoard(Board ingredientBoard) {
+//		this.ingredientBoard = ingredientBoard;
+//	}
+//	
+//	public void setDeductionBoard(Board deductionBoard) {
+//		this.deductionBoard = deductionBoard;
+//	}
 }
