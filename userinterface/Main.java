8579b3c2155fd8135f090827d2cedf8ae1a1a4ee
batuogkoc <bgunduz21@ku.edu.tr>
2023-12-03 14:55:37 +0300

    finished the initial implementation of the results triangle. Potion list to update the circles is needed, potion impl. is being waited upon

--- userinterface/Main.java
+++ userinterface/Main.java
@@ -1,6 +1,9 @@
 package userinterface;
 import java.awt.Dimension;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -14,10 +17,40 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		JPanel panel = new JPanel();
+		ResultsTriangle resultsTriangle = new ResultsTriangle();
+
+		JComboBox potionSelector1;
+		JComboBox potionSelector2;
+		JComboBox iconSelector;
+		
+		String[] potions = {"0","1","2","3","4","5","6","7"};
+		String[] icons = {"rm", "gm", "bm", "rp", "gp", "bp", "empty"};
+		JButton button = new JButton();
+		potionSelector1 = new JComboBox(potions);
+		potionSelector2 = new JComboBox(potions);
+		iconSelector = new JComboBox(icons);
+		potionSelector1.setMaximumSize(new Dimension(200, 40));
+		potionSelector2.setMaximumSize(new Dimension(200, 40));
+		iconSelector.setMaximumSize(new Dimension(200, 40));
+		
+		button.addActionListener(e -> {
+
+			resultsTriangle.setCircleImage(potionSelector1.getSelectedIndex(), potionSelector2.getSelectedIndex(), iconSelector.getSelectedIndex());
+
+		});
+		
+		panel.add(resultsTriangle);
+		panel.add(potionSelector1);
+		panel.add(potionSelector2);
+		panel.add(iconSelector);
+		panel.add(button);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
 		JFrame frame = new JFrame();
-		frame.add(new ResultsTriangle());
+		frame.add(panel);
 		frame.setVisible(true);
-		frame.setSize(400, 400);
+		frame.setSize(900, 700);
 	}
 
 
