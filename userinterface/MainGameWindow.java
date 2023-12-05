29c73ecd6acd4ff8b5374ecd24324d5b9ac17fe6
Doremina <mdurhasan21@ku.edu.tr>
2023-12-05 18:14:57 +0300

    3 buttons added in main

--- userinterface/MainGameWindow.java
+++ userinterface/MainGameWindow.java
@@ -14,6 +14,9 @@ import userinterface.util.GlobalDimensions;
 import userinterface.util.GlobalFonts;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import java.awt.Rectangle;
+import java.awt.Font;
+import java.awt.Component;
 
 public class MainGameWindow {
 
@@ -57,14 +60,67 @@ public class MainGameWindow {
 		contentPane.add(closeButton);
 
 		JPanel buttonsPanel = new JPanel();
-		buttonsPanel.setBounds(81, 147, 400, 270);
+		buttonsPanel.setBounds(81, 500, 315, 174);
 		contentPane.add(buttonsPanel);
 		buttonsPanel.setLayout(null);
 
-		JButton sellingredientButton = new JButton("Sell Ingredient");
-		sellingredientButton.setBounds(0, 0, 228, 259);
-		sellingredientButton.setIcon(new ImageIcon(MainGameWindow.class.getResource("/userinterface/images/flower_100x160.png")));
-		buttonsPanel.add(sellingredientButton);
+		JPanel transmuteIngredientPanel = new JPanel();
+		transmuteIngredientPanel.setBounds(0, 0, 105, 174);
+		buttonsPanel.add(transmuteIngredientPanel);
+		transmuteIngredientPanel.setLayout(null);
+
+		JButton sellIngredientButton = new JButton("Transmute Ingredient");
+		sellIngredientButton.setHorizontalTextPosition(SwingConstants.LEFT);
+		sellIngredientButton.setVerticalAlignment(SwingConstants.BOTTOM);
+		sellIngredientButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+		sellIngredientButton.setFont(GlobalFonts.ACTION_BUTTON);
+		sellIngredientButton.setBounds(transmuteIngredientPanel.getBounds());
+		sellIngredientButton.setContentAreaFilled(false); //TODO make it transparent
+		transmuteIngredientPanel.add(sellIngredientButton);
+
+		JLabel sellIngredientLabel = new JLabel();
+		sellIngredientLabel.setVerticalAlignment(SwingConstants.TOP);
+		sellIngredientLabel.setBounds(transmuteIngredientPanel.getBounds());
+		sellIngredientLabel.setIcon(new ImageIcon(MainGameWindow.class.getResource("/userinterface/images/flower_100x160.png")));
+		transmuteIngredientPanel.add(sellIngredientLabel);
+
+		JPanel buyArtifactPanel = new JPanel();
+		buyArtifactPanel.setBounds(transmuteIngredientPanel.getX()+transmuteIngredientPanel.getWidth(), 0, 105, 174);
+		buttonsPanel.add(buyArtifactPanel);
+		buyArtifactPanel.setLayout(null);
+
+		JButton buyArtifactButton = new JButton("Buy Artifact Card");
+		buyArtifactButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+		buyArtifactButton.setVerticalAlignment(SwingConstants.BOTTOM);
+		buyArtifactButton.setFont(GlobalFonts.ACTION_BUTTON);
+		buyArtifactButton.setContentAreaFilled(false);
+		buyArtifactButton.setBounds(0, 0, buyArtifactPanel.getWidth(), buyArtifactPanel.getHeight());
+		buyArtifactPanel.add(buyArtifactButton);
+
+		JLabel buyArtifactLabel = new JLabel();
+		buyArtifactLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+		buyArtifactLabel.setBounds(0, 0, buyArtifactPanel.getWidth(), buyArtifactPanel.getHeight());
+		buyArtifactPanel.add(buyArtifactLabel);
+
+		JPanel forageForIngredientPanel = new JPanel();
+		forageForIngredientPanel.setLayout(null);
+		forageForIngredientPanel.setBounds(buyArtifactPanel.getX()+buyArtifactPanel.getWidth(), 0, 105, 174);
+		buttonsPanel.add(forageForIngredientPanel);
+
+		JButton forageForIngredientButton = new JButton("Forage For Ingredient");
+		forageForIngredientButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+		forageForIngredientButton.setVerticalAlignment(SwingConstants.BOTTOM);
+		forageForIngredientButton.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		forageForIngredientButton.setContentAreaFilled(false);
+		forageForIngredientButton.setBounds(0, 0, 105, 174);
+		forageForIngredientPanel.add(forageForIngredientButton);
+
+		JLabel forageForIngredientLabel = new JLabel();
+		forageForIngredientLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+		forageForIngredientLabel.setBounds(0, 0, 105, 174);
+		forageForIngredientPanel.add(forageForIngredientLabel);
+
+
 
 
 
