e8411ace07f834dc6cd7e045d865a63d73d1be6d
azadaslanli <aaslanli21@ku.edu.tr>
2023-12-06 18:24:48 +0300

    Parameters fixed for brewPotion and transmuteIngredient

--- domain/boards/BoardController.java
+++ domain/boards/BoardController.java
@@ -15,7 +15,15 @@ public class BoardController {
 	
 	BoardController(){}
 	
-	public static void transmuteIngredient(IngredientCard ingr) throws UserErrorException {
+	public static void transmuteIngredient(String ingredient) throws UserErrorException {
+		//Finding proper Ingredient with given name
+		
+		IngredientCard ingr = null;
+		for (IngredientCard ingr_1 : GameController.getGameInventory().getIngredientCards()){
+    		if (ingr_1.getName() == ingredient) {
+    			ingr = ingr_1;}
+    	}
+		
 		ingrBoard.transmuteIngredient(ingr);
 	}
 	
@@ -31,8 +39,17 @@ public class BoardController {
 		pubBoard.debunkTheory(GameController.getCurrentPlayer(), hypothesis);
 	}
 	
-	public static String brewPotion(String ing1, String ing2, boolean onStu) throws UserErrorException{
-		return potBoard.makeExperiment(ing1, ing2, onStu);
+	public static String brewPotion(String ingredient1, String ingredient2, boolean onStu) throws UserErrorException{
+		//Find ingredients with given names  	
+       	IngredientCard ingr1 = null;
+		IngredientCard ingr2 = null;
+		for (IngredientCard ingredient : GameController.getGameInventory().getIngredientCards()){
+    		if (ingredient.getName() == ingredient1) {
+    			ingr1 = ingredient;}
+    		if (ingredient.getName() == ingredient2) {
+        		ingr2 = ingredient;}
+    	}
+		return potBoard.makeExperiment(ingr1, ingr2, onStu);
 	}
 
 }
