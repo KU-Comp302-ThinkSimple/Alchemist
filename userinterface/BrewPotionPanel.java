692b4cb6c583c769f3cddf3727e22a3b0dc54c54
Doremina <mdurhasan21@ku.edu.tr>
2023-12-06 19:56:54 +0300

    edited choices and added button listeners.
    back functions are called in test on student and drink potion button listeners

--- userinterface/BrewPotionPanel.java
+++ userinterface/BrewPotionPanel.java
@@ -60,12 +60,79 @@ public class BrewPotionPanel extends JPanel {
 		add(drinkPotionPanel);
 		drinkPotionPanel.setLayout(null);
 
+
+		Choice choice1 = new Choice();
+		choice1.setForeground(GlobalColors.BUTTON_COLOR);
+		choice1.setBackground(new Color(100, 0, 0));
+		choice1.setFont(new Font("Felix Titling", Font.BOLD, 14)); //TODO GlobalFonts
+		choice1.setBounds(10, 234, 230, 20);
+		add(choice1);
+
+
+		choice1.add("Please select an ingredient");
+		choice1.add("raven's feather");
+		choice1.add("mandrake root");
+		choice1.add("mushroom");
+		choice1.add("frog");
+		choice1.add("flower");
+		choice1.add("scorpion");
+		choice1.add("bird claw");
+		choice1.add("seedling");
+
+
+		Choice choice2 = new Choice();
+		choice2.setForeground(new Color(196, 133, 2));
+		choice2.setBackground(new Color(85, 0, 0));
+		choice2.setFont(new Font("Felix Titling", Font.BOLD, 14));
+		choice2.setBounds(266, 234, 230, 20);
+		add(choice2);
+
+
+		choice2.add("Please select an ingredient");
+		choice2.add("raven's feather");
+		choice2.add("mandrake root");
+		choice2.add("mushroom");
+		choice2.add("frog");
+		choice2.add("flower");
+		choice2.add("scorpion");
+		choice2.add("bird claw");
+		choice2.add("seedling");
+
+
+		//TODO get inventory items and add them to choice1 and choice 2
+		/*
+		int item_count = GameController.getCurrentPlayer().getInventory().getPlayerIngredientCardList().size();
+		for (int i = 0; i < item_count; i++) {
+			String cardName = GameController.getCurrentPlayer().getInventory().getPlayerIngredientCardList().get(i).getName();
+			choice1.add(cardName);
+			choice2.add(cardName);
+		}
+		 */
+
+
+
+		/*
+		 if (choice1.chosen) {
+		 	choice2.remove(choice1.getSelectedItem());
+		 }
+		 */
+
+
 		JButton drinkPotionButton = new JButton("Drink Potion");
 		drinkPotionButton.setBounds(22, 231, 180, 80);
-		drinkPotionPanel.add(drinkPotionButton);
 		drinkPotionButton.setForeground(GlobalColors.BUTTON_TEXT_COLOR);
 		drinkPotionButton.setBackground(GlobalColors.BUTTON_COLOR);
 		drinkPotionButton.setFont(GlobalFonts.BREW_BUTTON);
+		drinkPotionButton.addActionListener(e -> {
+			try {
+				String message = GameController.getBoard().getPotionBrewingBoard().makeExperiment(choice1.getSelectedItem(), choice2.getSelectedItem(), false);
+				JOptionPane.showMessageDialog(this, "You brew: " + message);
+			}
+			catch (Exception error) {
+				JOptionPane.showMessageDialog(this, error.getMessage());
+			}
+		});
+		drinkPotionPanel.add(drinkPotionButton);
 
 		JLabel drinkPotionLabel = new JLabel(new ImageIcon(BrewPotionPanel.class.getResource("/userinterface/images/drinkPotion_247x285.png")));
 		drinkPotionLabel.setBounds(0, 0, 247, 285);
@@ -86,10 +153,19 @@ public class BrewPotionPanel extends JPanel {
 
 		JButton testOnStudentButton = new JButton("Test on Student");
 		testOnStudentButton.setBounds(22, 233, 180, 80);
-		testOnStudentPanel.add(testOnStudentButton);
 		testOnStudentButton.setForeground(GlobalColors.BUTTON_TEXT_COLOR);
 		testOnStudentButton.setBackground(GlobalColors.BUTTON_COLOR);
 		testOnStudentButton.setFont(GlobalFonts.BREW_BUTTON);
+		testOnStudentButton.addActionListener(e -> {
+			try {
+				String message = GameController.getBoard().getPotionBrewingBoard().makeExperiment(choice1.getSelectedItem(), choice2.getSelectedItem(), true);
+				JOptionPane.showMessageDialog(this, "You brew: " + message);
+			}
+			catch (Exception error) {
+				JOptionPane.showMessageDialog(this, error.getMessage());
+			}
+		});
+		testOnStudentPanel.add(testOnStudentButton);
 
 		JLabel testOnStudentLabel=new JLabel(new ImageIcon(BrewPotionPanel.class.getResource("/userinterface/images/testOnStudent_247x285.png")));
 		testOnStudentLabel.setBounds(0, 0, 247, 285);
@@ -112,51 +188,6 @@ public class BrewPotionPanel extends JPanel {
 		JLabel sellAPotionLabel=new JLabel(new ImageIcon(BrewPotionPanel.class.getResource("/userinterface/images/sellAPotion_247x285.png")));
 		sellAPotionLabel.setBounds(0, 0, 247, 285);
 		sellAPotionPanel.add(sellAPotionLabel);
-
-		Choice choice1 = new Choice();
-		choice1.setForeground(GlobalColors.BUTTON_COLOR);
-		choice1.setBackground(new Color(100, 0, 0));
-		choice1.setFont(new Font("Felix Titling", Font.BOLD, 14)); //TODO GlobalFonts
-		choice1.setBounds(10, 234, 230, 20);
-		add(choice1);
-
-		/*
-		choice1.add("Please select an ingredient");
-		choice1.add("raven's feather");
-		choice1.add("mandrake root");
-		choice1.add("mushroom");
-		choice1.add("frog");
-		choice1.add("flower");
-		choice1.add("scorpion");
-		choice1.add("bird claw");
-		choice1.add("seedling");
-		 */
-
-		//TODO get inventory items and add them to choice
-		int item_count = GameController.getCurrentPlayer().getInventory().getPlayerIngredientCardList().size();
-		String[] inventory = new String[item_count];
-		for (int i = 0; i < item_count; i++) {
-			inventory[i] = GameController.getCurrentPlayer().getInventory().getPlayerIngredientCardList().get(i).getName();
-			choice1.add(inventory[i]);
-		}
-
-
-		Choice choice2 = new Choice();
-		choice2.setForeground(new Color(196, 133, 2));
-		choice2.setBackground(new Color(85, 0, 0));
-		choice2.setFont(new Font("Felix Titling", Font.BOLD, 14));
-		choice2.setBounds(266, 234, 230, 20);
-		add(choice2);
-
-		choice2.add("Please select an ingredient");
-		choice2.add("raven's feather");
-		choice2.add("mandrake root");
-		choice2.add("mushroom");
-		choice2.add("frog");
-		choice2.add("flower");
-		choice2.add("scorpion");
-		choice2.add("bird claw");
-		choice2.add("seedling");
 
 		JLabel potionLabel=new JLabel(new ImageIcon(BrewPotionPanel.class.getResource("/userinterface/images/unknownPotion_103x151.png")));
 		potionLabel.setBounds(537, 72, 103, 151);
