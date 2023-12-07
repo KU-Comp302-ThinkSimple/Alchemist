b7382dfe8b3a16947523293bc0bb5469e5a6440f
batuogkoc <bgunduz21@ku.edu.tr>
2023-12-07 23:50:32 +0300

    added game info string

--- userinterface/MainGameWindow.java
+++ userinterface/MainGameWindow.java
@@ -36,6 +36,39 @@ import java.util.ArrayList;
 import javax.swing.JComboBox;
 
 public class MainGameWindow {
+	private static String infoText = """
+Welcome to Alchemy Lab Game!
+
+In this game, you take on the role of an alchemist conducting experiments in the lab. Your goal is to brew potions, contribute to publications, and form theories about ingredient properties. Here's a quick guide to get you started:
+
+Game Phases:
+
+First Round:
+Forage for Ingredients: Draw ingredients from the deck.
+Transmute Ingredient: Discard an ingredient for gold.
+Buy Artifacts: Purchase artifacts with gold.
+Make Experiments: Mix ingredients, test potions, and gain results.
+Second Round:
+Sell a Potion: Offer potions to adventurers for gold.
+Publish a Theory: Share your knowledge about ingredients for reputation points.
+Final Round:
+Debunk or Endorse Theories: Prove or disprove published theories for reputation points.
+Game Elements:
+
+Player Tokens: Represent your unique avatar. Track your position, resources, and scores.
+Ingredients: Various types with unique properties. Store them in the Ingredient Storage.
+Potions: Brew potions with specific recipes and point values.
+Publication Cards: Contribute to theories for reputation points.
+Artifact Cards: Purchase for unique game advantages.
+Alchemy Markers: Form theories on the Deduction Board.
+Winning the Game:
+
+Accumulate reputation points and gold.
+Exchange leftover artifacts for gold.
+Score one-third of a point for each gold piece.
+The player with the most score points wins!
+Good luck, alchemist! May your potions be potent and your theories groundbreaking!
+""";
 
 	private JPanel contentPane;
 	private JPanel deductionBoard = new DeductionBoard();
@@ -216,7 +249,7 @@ public class MainGameWindow {
 		infoButton.setBounds(10, 61, 60, 39);
 		infoButton.addActionListener(e -> {
 			//TODO new information
-			JOptionPane.showMessageDialog(MainGameWindowFrame, "Taktik maktik yok bam bam bam.");
+			JOptionPane.showMessageDialog(MainGameWindowFrame, infoText);
 		}
 				);
 		contentPane.add(infoButton);
