66f4ba4e6e3b580fab3d0968c750fac65be6b281
Doremina <mdurhasan21@ku.edu.tr>
2023-12-27 19:49:08 +0300

    still working. just to make sure the code is safe

--- test/TestMakeExperiment.java
+++ test/TestMakeExperiment.java
@@ -7,6 +7,7 @@ import domain.GameInventory;
 import domain.cards.IngredientCard;
 import domain.potion.Atom;
 import domain.potion.Molecule;
+import exception.UserErrorException;
 import userinterface.LoginSignupController;
 
 public class TestMakeExperiment {
@@ -21,14 +22,39 @@ public class TestMakeExperiment {
 
 	public void testExperiment() {
 
+		//Initialize game first
+		controller = GameController.getInstance();
+
 		LoginSignupController.getInstance().signup("TestUser1", "testuser");
 		LoginSignupController.getInstance().login("TestUser1", "testuser");
 
 		LoginSignupController.getInstance().signup("TestUser2", "testuser");
 		LoginSignupController.getInstance().login("TestUser2", "testuser");
 
-		GameController.initializeGame();
-		//controller = GameController.geti
-		inventory = GameInventory.getInstance();
+		controller.initializeGame();
+
+		//Then we can get inventory (after the game is initialized)
+		inventory = controller.getGameInventory();
+
+		//makeExperiment(IngredientCard ingredient1, IngredientCard ingredient2, Boolean testOnStu)
+		//throws UserErrorException, RuntimeException
+
+		//For testing purposes, added 50 actions to the current player
+		controller.getCurrentPlayer().getPlayerToken().setPlayerAction(50);
+
+		/*
+		 * Things to test:
+		 **** For all tests
+		 **
+		 **** Drink Potion Tests
+		 ** Health Potion (Red +) -> playerHealth should increase by 1.
+		 ** Speed Potion (Green +) -> playerAction should increase by 1.
+		 ** Wisdom Potion (Blue +) -> playerReputation should increase by 1.
+		 *                           (Since player's gonna use 1 action to test the potion, the action amount before and after...
+		 * 							 ...makeExperiment should remain the same.)
+		 *
+		 */
+
+
 	}
 }
