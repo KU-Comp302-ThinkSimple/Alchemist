5c3d11e81cf72a370c81d20a4471cf9b5e9a656f
Doremina <mdurhasan21@ku.edu.tr>
2023-12-29 00:24:45 +0300

    commented out problematic code

--- test/TestMakeExperiment.java
+++ test/TestMakeExperiment.java
@@ -17,8 +17,8 @@ public class TestMakeExperiment {
 	ArrayList<IngredientCard> ingredientCards;
 
 	public static void main(String[] args) {
-		testExperiment();
-		System.out.print(controller.getCurrentPlayer());
+		//testExperiment();
+		//System.out.print(controller.getCurrentPlayer());
 	}
 
 	public void testExperiment() {
