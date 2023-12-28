0afff62f359b16044aee986c76f9e79b3561fc7f
Özgür MENTEŞE <ozgur.mentese@drd.com.tr>
2023-12-29 01:33:59 +0300

    testing cont. with learning JUnit

--- test/TestPublishTheory.java
+++ test/TestPublishTheory.java
@@ -1,13 +1,43 @@
 package test;
+import domain.GameController;
+import domain.GameInventory;
 import domain.boards.PublicationBoard;
 
+import domain.player.Player;
+import domain.theory.Hypotheses;
 import org.junit.jupiter.api.Test;
+import userinterface.LoginSignupController;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestPublishTheory {
+    private final GameController controller = GameController.getInstance();
+    private final LoginSignupController loginSignupController = LoginSignupController.getInstance();
     private final PublicationBoard publicationBoard = new PublicationBoard();
+    private final GameInventory gameInventory = GameInventory.getInstance();
+
+    private ArrayList<Hypotheses> testHypotheses = new ArrayList<>();
+
+    TestPublishTheory(){
+        loginSignupController.signup("publishTheoryTesterUser1", "");
+        loginSignupController.login("publishTheoryTesterUser1", "");
+
+        loginSignupController.signup("publishTheoryTesterUser2", "");
+        loginSignupController.login("publishTheoryTesterUser2", "");
+
+        controller.initializeGame();
+    }
 
     @Test
-    public void publishTheory(){
+    public void testRoundNumberNotToBe1(){
+        assertFalse(controller.getCurrentRound()==1);
+    }
 
+    @Test
+    public void testRoundNumberToBe2Or3(){
+        assertTrue(controller.getCurrentRound()==2 || controller.getCurrentRound()==3);
     }
 }
