c510ff800bb0d0fc6e7fc1d58c48c32c7402b99d
Doremina <mdurhasan21@ku.edu.tr>
2023-12-28 17:19:18 +0300

    can't test tostring methods due to classpath error

--- test/TestMakeExperiment.java
+++ test/TestMakeExperiment.java
@@ -17,7 +17,8 @@ public class TestMakeExperiment {
 	ArrayList<IngredientCard> ingredientCards;
 
 	public static void main(String[] args) {
-
+		testExperiment();
+		System.out.print(controller.getCurrentPlayer());
 	}
 
 	public void testExperiment() {
@@ -39,8 +40,8 @@ public class TestMakeExperiment {
 		//makeExperiment(IngredientCard ingredient1, IngredientCard ingredient2, Boolean testOnStu)
 		//throws UserErrorException, RuntimeException
 
-		//For testing purposes, added 50 actions to the current player
-		controller.getCurrentPlayer().getPlayerToken().setPlayerAction(50);
+		//For testing purposes, added 100 actions to the current player
+		controller.getCurrentPlayer().getPlayerToken().setPlayerAction(100);
 
 		/*
 		 * Things to test:
@@ -52,8 +53,20 @@ public class TestMakeExperiment {
 		 ** Wisdom Potion (Blue +) -> playerReputation should increase by 1.
 		 *                           (Since player's gonna use 1 action to test the potion, the action amount before and after...
 		 * 							 ...makeExperiment should remain the same.)
+		 * 
+		 ** Poison Potion (Red -) -> playerHealth should reduce by 1.
+		 ** Slow Potion (Green -) -> playerHealth should reduce by 1. playerAction should reduce by 1 (if any left).
+		 ** Insanity Potion (Blue -) -> playerHealth should reduce by 1. playerReputation should reduce by 1.
 		 *
+		 ** Neutral Potion -> No effect.
 		 */
+
+		//TODO In Potion class's testPotion() method, line 61
+		//if (!GameController.getInstance().getCurrentPlayer().getInventory().getPlayerArtifactCardList().contains(GameController.getInstance().getGameInventory().getArtCards().get(1)))
+		//is a very fragile code. It should check all items in ArrayList and check if (item instanceof Vaccine)
+		//when the order of the cards change, the function won't function
+		//TODO playerHealth is in both Player and PlayerToken classes. Choose one (I think Player class is a better answer)
+
 
 
 	}
