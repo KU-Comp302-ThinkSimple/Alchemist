8ac8538d2896442a87612b2162c7c4cce9208ef5
Doremina <mdurhasan21@ku.edu.tr>
2023-12-31 15:06:18 +0300

    creation of test class

--- test/TestGameInitializer.java
+++ test/TestGameInitializer.java
@@ -8,14 +8,14 @@ public class TestGameInitializer {
 	public TestGameInitializer() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public static void initializeTestGame() {
-		LoginSignupController.getInstance().signup("user_1", "user_1");
-		LoginSignupController.getInstance().signup("user_2", "user_2");
-		
-		LoginSignupController.getInstance().login("user_1", "user_1");
-		LoginSignupController.getInstance().login("user_2", "user_2");
-		
+		LoginSignupController.getInstance().signup("TestUser1", "user_1");
+		LoginSignupController.getInstance().signup("TestUser2", "user_2");
+
+		LoginSignupController.getInstance().login("TestUser1", "user_1");
+		LoginSignupController.getInstance().login("TestUser2", "user_2");
+
 		GameController.getInstance().initializeGame();
 	}
 }
