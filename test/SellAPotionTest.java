d83e77f2559ed7d2f39cabe97383141fa8679a6f
Doremina <mdurhasan21@ku.edu.tr>
2023-12-31 17:32:13 +0300

    positiveGuaranteeFulfilled doesn't pass tests

--- test/SellAPotionTest.java
+++ test/SellAPotionTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +16,7 @@ import domain.cards.IngredientCard;
 import domain.player.Player;
 import domain.potion.Molecule;
 import domain.potion.Potion;
+import domain.potion.Recipe;
 import exception.UserErrorException;
 
 class SellAPotionTest {
@@ -41,12 +43,13 @@ class SellAPotionTest {
 		player = GameController.getInstance().getCurrentPlayer();
 
 		//Add actions to the current player (so we'll not change turns and keep testing one player)
-		player.getPlayerToken().setPlayerAction(50);
+		player.getPlayerToken().setPlayerAction(100);
 
 		//Make sure it's round 2+
 		GameController.getInstance().changeRounds(); // now we're in round2
 
 	}
+
 
 	@AfterEach
 	void removeActivePlayers() {
@@ -96,11 +99,51 @@ class SellAPotionTest {
 
 	@Test
 	void positiveGuaranteeFulfilled() {
+		//Molecules 0 and 3 create a Speed Potion (Green +)
+		//Mol 0 = sR- sG+ bB- (Molecule of ingr(0))
+		//Mol 3 = sR- bG+ sB+ (Molecule of ingr(1))
 
+		ArrayList<IngredientCard> ingrs = GameController.getInstance().getGameInventory().getIngrCards();
+		IngredientCard ingr0 = ingrs.get(0);
+		IngredientCard ingr3 = ingrs.get(3);
+
+		//Double check corresponding molecules create Green + pot
+		Assertions.assertEquals(new Recipe(ingr0, ingr3).checkGreenMatch(), 1);
+
+		//Add cards to player's inventory
+		player.getInventory().getPlayerIngredientCardList().add(ingr0);
+		player.getInventory().getPlayerIngredientCardList().add(ingr3);
+
+		//Player's gold and card stats before selling a potion
+		int moneyBeforeSell = player.getPlayerToken().getGold();
+		System.out.println(moneyBeforeSell); //TODO
+		ArrayList<IngredientCard> cardsBeforeSell = player.getPlayerToken().getPlayerInventory().getPlayerIngredientCardList();
+
+		//Sell potion with ingr0, ingr3, 2 (positive guaranteed)
+		try {
+			GameController.getInstance().getBoard().getPotionBrewingBoard().sellPotion(ingr0, ingr3, 2);
+		}
+		catch (UserErrorException e) {
+			Assert.fail("sellPotion method threw UserErrorException with message: \n" + e.getMessage());
+		}
+
+
+		//Check ingr card inventory before and after
+		ArrayList<IngredientCard> cardsAfterSell = player.getPlayerToken().getPlayerInventory().getPlayerIngredientCardList();
+		cardsBeforeSell.remove(ingr0);
+		cardsBeforeSell.remove(ingr3);
+		Assertions.assertEquals(cardsBeforeSell, cardsAfterSell);
+
+		//Check money before and after
+		int moneyAfterSell = player.getPlayerToken().getGold();
+		System.out.println(moneyAfterSell);
+		Assertions.assertEquals(moneyBeforeSell + 3, moneyAfterSell);
+
+		//check recipes list?
 	}
 
 	@Test
-	void nonNegativeGuaranteeFulfilled() {
+	void positiveGuaranteeNotFulfilled() {
 
 	}
 
