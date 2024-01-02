4fe7031757b88a03df5f23d171b62f22a6069bae
batuogkoc <bgunduz21@ku.edu.tr>
2024-01-02 21:20:38 +0300

    Implemented the carddeck adt.

--- domain/cards/artifactCards/ElixirOfInsight.java
+++ domain/cards/artifactCards/ElixirOfInsight.java
@@ -13,25 +13,21 @@ public class ElixirOfInsight extends ArtifactCard{
 		
 	}
 
-	public static ArrayList<IngredientCard> shuffle(ArrayList<IngredientCard> cards, int[] order){
-		ArrayList<IngredientCard> newCards = new ArrayList<IngredientCard>();
-		newCards.add(cards.get(order[0]-1));
-		newCards.add(cards.get(order[1]-1));
-		newCards.add(cards.get(order[2]-1));
-		return newCards;
-	}
+//	public static ArrayList<IngredientCard> shuffle(ArrayList<IngredientCard> cards, int[] order){
+//		ArrayList<IngredientCard> newCards = new ArrayList<IngredientCard>();
+//		newCards.add(cards.get(order[0]-1));
+//		newCards.add(cards.get(order[1]-1));
+//		newCards.add(cards.get(order[2]-1));
+//		return newCards;
+//	}
 
 	
 	public ArrayList<IngredientCard> changeCards(int [] order) {
 		ArrayList<IngredientCard> topThreeCards =this.useCard();
+		
+		GameController.getInstance().getBoard().getIngredientBoard().getIngredientDeck().reorder(order);
 
-		ArrayList<IngredientCard> newTopThreeCards = shuffle(topThreeCards, order);
-
-		for (int i = 0; i<3; i++){
-			GameController.getInstance().getBoard().getIngredientBoard().getIngredientList().set(i, newTopThreeCards.get(i));
-		}
-
-		return newTopThreeCards;
+		return GameController.getInstance().getBoard().getIngredientBoard().getIngredientDeck().getCards();
 
 	}
 
