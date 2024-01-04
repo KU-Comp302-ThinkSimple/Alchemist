0f8e7414be7b3926e70f351c16223c3867cd0863
mmentese21 <mmentese21@ku.edu.tr>
2024-01-05 02:23:57 +0300

    Artifact Cards are refactored accordingly to the new RandomDeck abstract class. versioned userinfosaver

--- domain/cards/artifactCards/ElixirOfInsight.java
+++ domain/cards/artifactCards/ElixirOfInsight.java
@@ -1,34 +1,38 @@
 package domain.cards.artifactCards;
 
-import java.util.ArrayList;
-
 import domain.GameController;
 import domain.cards.IngredientCard;
+import domain.cards.artifactCards.behaviors.ArtifactCardBehavior;
 
-public class ElixirOfInsight extends ArtifactCard{
+import java.util.ArrayList;
 
-	
-	public ElixirOfInsight(ArtifactEffectBehavior effect, String name) {
+public class ElixirOfInsight extends ArtifactCard<ArrayList<IngredientCard>> {
+
+
+	public ElixirOfInsight(ArtifactCardBehavior effect, String name) {
 		super(effect, name);
-		
-	}
-
-//	public static ArrayList<IngredientCard> shuffle(ArrayList<IngredientCard> cards, int[] order){
-//		ArrayList<IngredientCard> newCards = new ArrayList<IngredientCard>();
-//		newCards.add(cards.get(order[0]-1));
-//		newCards.add(cards.get(order[1]-1));
-//		newCards.add(cards.get(order[2]-1));
-//		return newCards;
-//	}
-
-	
-	public ArrayList<IngredientCard> changeCards(int [] order) {
-		ArrayList<IngredientCard> topThreeCards =this.useCard();
-		
-		GameController.getInstance().getBoard().getIngredientBoard().getIngredientDeck().reorder(order);
-
-		return GameController.getInstance().getBoard().getIngredientBoard().getIngredientDeck().getCards();
 
 	}
+
+	public static ArrayList<IngredientCard> shuffle(ArrayList<IngredientCard> cards, int[] order) {
+		ArrayList<IngredientCard> newCards = new ArrayList<IngredientCard>();
+		newCards.add(cards.get(order[0] - 1));
+		newCards.add(cards.get(order[1] - 1));
+		newCards.add(cards.get(order[2] - 1));
+		return newCards;
+	}
+
+
+	public void changeCards(int[] order) {
+		ArrayList<IngredientCard> topThreeCards = this.useCard();
+
+		ArrayList<IngredientCard> newTopThreeCards = shuffle(topThreeCards, order);
+
+		for (int i = 0; i < 3; i++) {
+			GameController.getInstance().getBoard().getIngredientBoard().getIngredientDeck().getCards().set(i, newTopThreeCards.get(i));
+		}
+
+	}
+
 
 }
--- domain/cards/artifactCards/behaviors/ElixirOfInsightBehavior.java
+++ domain/cards/artifactCards/behaviors/ElixirOfInsightBehavior.java
@@ -1,15 +1,15 @@
-package domain.cards.artifactCards;
+package domain.cards.artifactCards.behaviors;
 
 import domain.GameController;
 import domain.cards.IngredientCard;
 
 import java.util.ArrayList;
 
-public class ElixirOfInsightEffect implements ArtifactEffectBehavior{
+public class ElixirOfInsightBehavior implements ArtifactCardBehavior<ArrayList<IngredientCard>> {
 
-	
+	@Override
 	public ArrayList<IngredientCard> use() {
-		
+
 		IngredientCard card1 = GameController.getInstance().getBoard().getIngredientBoard().getIngredientDeck().getCards().get(0);
 		IngredientCard card2 = GameController.getInstance().getBoard().getIngredientBoard().getIngredientDeck().getCards().get(1);
 		IngredientCard card3 = GameController.getInstance().getBoard().getIngredientBoard().getIngredientDeck().getCards().get(2);
@@ -18,11 +18,8 @@ public class ElixirOfInsightEffect implements ArtifactEffectBehavior{
 		topThreeCards.add(card2);
 		topThreeCards.add(card3);
 
-		
+
 		return topThreeCards;
 
 	}
-
-
-
 }
