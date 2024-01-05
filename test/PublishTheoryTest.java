03bac116b0362cc6cdf2fb117148dfa8a93e7768
mmentese21 <mmentese21@ku.edu.tr>
2024-01-05 04:43:29 +0300

    deleted wrong tests

--- test/PublishTheoryTest.java
+++ test/PublishTheoryTest.java
@@ -1,33 +1,21 @@
 package test;
 
 import domain.GameController;
-import domain.GameInventory;
-import domain.boards.PublicationBoard;
-import domain.theory.Hypotheses;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.TestInstance;
-import userinterface.LoginSignupController;
 
-import java.util.ArrayList;
-
-import static org.junit.Assert.assertFalse;
+import static test.TestGameInitializer.initializeTestGame;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PublishTheoryTest {
-    private final GameController controller = GameController.getInstance();
-    private final LoginSignupController loginSignupController = LoginSignupController.getInstance();
-    private final PublicationBoard publicationBoard = new PublicationBoard();
-    private final GameInventory gameInventory = GameInventory.getInstance();
-
-    private ArrayList<Hypotheses> testHypotheses = new ArrayList<>();
-
-    // JUnit 5 setup before all tests
+    private GameController gameController;
     @BeforeAll
     public void setUpBeforeAll() {
+        initializeTestGame();
+        gameController = GameController.getInstance();
 
-        controller.initializeGame();
     }
 
     /*
@@ -37,8 +25,7 @@ public class PublishTheoryTest {
     @Test
     @DisplayName("Test round number.")
     public void testRoundNumber(){
-        int currentRound = controller.getCurrentRound();
-        assertFalse("Player should be in round 2 or 3 to publish a theory.", currentRound!=1);
+
     }
 
     /*
@@ -49,7 +36,6 @@ public class PublishTheoryTest {
     @Test
     @DisplayName("Test player gold.")
     public void testPlayerGold(){
-        assertFalse("Player should have at least 1 gold to publish a theory",controller.getCurrentPlayer().getPlayerToken().getGold() < 1);
     }
 
 
