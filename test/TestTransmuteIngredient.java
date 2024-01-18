885e5a8b4ba735cc4ec257d225d4a98b43f81121
larakucukdevlet <79307181+larakucukdevlet@users.noreply.github.com>
2024-01-18 11:47:58 +0300

    Update TestTransmuteIngredient.java

--- test/TestTransmuteIngredient.java
+++ test/TestTransmuteIngredient.java
@@ -1,17 +1,21 @@
 package test;
+import static org.junit.jupiter.api.Assertions.*;
 
-/*import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;*/
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import domain.GameController;
-import domain.boards.BoardController;
+
 import domain.cards.IngredientCard;
 import domain.player.Player;
 import exception.UserErrorException;
 
-
-public class transmuteIngredientTest {
-	/*Test Cases:
+/*Test Cases:
 	Black-Box Test: Transmuting Ingredient Card owned by current player
 	Scenario: Providing an ingredient card owned by the player to transmute.
 	Expected result: The method successfully transmutes the ingredient card to 1 gold and reduces the player's action count.
@@ -31,27 +35,92 @@ public class transmuteIngredientTest {
 	Glass-Box Test: Player Action Reduction
 	Scenario: Checking the player action reduction.
 	Expected result: The player's action count decreases by 1 after transmutation.*/
-	
-	//@Test
-    public void testTransmuteOwnedIngredient() throws UserErrorException {
-        // Test: Transmuting an owned ingredient card
-		GameController.getGameInventory().createAtom();
-		GameController.getGameInventory().createMolecule();
-		GameController.getGameInventory().createIngredientCard();
-		
-		IngredientCard ownedCard=GameController.getGameInventory().getIngrCards().get(0);
-		
-        Player currentPlayer = new Player(1, "aa", "bb");
-        currentPlayer.getInventory().addAIngredientCard(ownedCard);
-        currentPlayer.getPlayerToken().setPlayerAction(3);
-        currentPlayer.getPlayerToken().setGold(5);
-        BoardController.transmuteIngredient(ownedCard.getName());
+public class TransmuteIngredientTest {
+       Player player;	
+       
+       
+       @BeforeEach
+       void TestInitialize() {
+    	   TestGameInitializer.initializeTestGame();
+           player = GameController.getInstance().getCurrentPlayer();
+           player.getPlayerToken().setPlayerAction(100);
+           GameController.getInstance().changeRounds();
+       }
+       @AfterEach
+       void removeActivePlayers() {
+           GameController.getInstance().getActivePlayers().clear();
+       }
 
-        // Assert
-        /*assertEquals(4, currentPlayer.getPlayerToken().getPlayerAction());
-        assertEquals(6, currentPlayer.getPlayerToken().getGold());
-        assertFalse(currentPlayer.getInventory().getPlayerIngredientCardList().contains(ownedCard));*/
-    }
+       @Test
+       void transmuteOwnedIngredientCard() { //	Expected result: The method successfully transmutes the ingredient card to 1 gold and reduces the player's action count.
+           ArrayList<IngredientCard> ingrs = GameController.getInstance().getGameInventory().getIngrCards();
+           IngredientCard ownedIngredient = ingrs.get(0);
 
+           player.getInventory().getPlayerIngredientCardList().add(ownedIngredient);
 
+           int initGold = player.getPlayerToken().getGold();
+           int initAct = player.getPlayerToken().getPlayerAction();
+
+           assertDoesNotThrow(() -> GameController.getInstance().getBoard().getIngredientBoard().transmuteIngredient(ownedIngredient));
+
+           int finalGold = player.getPlayerToken().getGold();
+           int finalAct = player.getPlayerToken().getPlayerAction();
+
+           Assertions.assertEquals(initGold + 1, finalGold); //increase gold
+           Assertions.assertEquals(initAct - 1, finalAct);//reduce action count
+           Assertions.assertFalse(player.getInventory().getPlayerIngredientCardList().contains(ownedIngredient));
+       }
+
+       @Test
+       void transmuteNonOwnedIngredientCard() {//The method throws a UserErrorException since the ingredient card is not owned by the player.
+           ArrayList<IngredientCard> ingredients = GameController.getInstance().getGameInventory().getIngrCards();
+           IngredientCard nonOwnedIngredient = ingredients.get(0);
+	   int initGold = player.getPlayerToken().getGold();
+           int initActions = player.getPlayerToken().getPlayerAction();
+
+           Throwable exception = assertThrows(UserErrorException.class, () ->
+                   GameController.getInstance().getBoard().getIngredientBoard().transmuteIngredient(nonOwnedIngredient)
+           );
+
+           Assertions.assertEquals("Cannot transmute a non-owned ingredient card.", exception.getMessage());
+	   int finalGold = player.getPlayerToken().getGold();
+           int finalAct = player.getPlayerToken().getPlayerAction();
+
+           Assertions.assertEquals(initGold, finalGold);
+           Assertions.assertEquals(initActions, finalAct);
+       }
+
+       @Test
+       void checkInventoryModification() { //ingredient card is removed from the player's inventory after transmutation.
+           ArrayList<IngredientCard> ingredients = GameController.getInstance().getGameInventory().getIngrCards();
+           IngredientCard ingredientToTransmute = ingredients.get(0);
+	    player.getInventory().getPlayerIngredientCardList().add(ingredientToTransmute);
+	    assertDoesNotThrow(() -> GameController.getInstance().getBoard().getIngredientBoard().transmuteIngredient(ingredientToTransmute));
+            Assertions.assertFalse(player.getInventory().getPlayerIngredientCardList().contains(ingredientToTransmute));
+       }
+
+       @Test
+       void checkGoldAddition() {
+           ArrayList<IngredientCard> ingredients = GameController.getInstance().getGameInventory().getIngrCards();
+           IngredientCard ingredientToTransmute = ingredients.get(0);
+	   player.getInventory().getPlayerIngredientCardList().add(ingredientToTransmute);
+	   int initGold = player.getPlayerToken().getGold();
+
+           //assertDoesNotThrow(() -> GameController.getInstance().getBoard().transmuteIngredient(ingredientToTransmute));
+           assertDoesNotThrow(() -> GameController.getInstance().getBoard().getIngredientBoard().transmuteIngredient(ingredientToTransmute));
+           int finalGold = player.getPlayerToken().getGold();
+	   Assertions.assertEquals(initGold + 1, finalGold);
+       }
+
+       @Test
+       void checkPlayerActionReduction() {
+           ArrayList<IngredientCard> ingrs = GameController.getInstance().getGameInventory().getIngrCards();
+           IngredientCard ingredientToTransmute = ingrs.get(0);
+	   player.getInventory().getPlayerIngredientCardList().add(ingredientToTransmute);
+	   int initAct = player.getPlayerToken().getPlayerAction();
+           assertDoesNotThrow(() -> GameController.getInstance().getBoard().getIngredientBoard().transmuteIngredient(ingredientToTransmute));
+ 	   int finalAct = player.getPlayerToken().getPlayerAction();
+	   Assertions.assertEquals(initAct - 1, finalAct);
+       }
+       
 }
