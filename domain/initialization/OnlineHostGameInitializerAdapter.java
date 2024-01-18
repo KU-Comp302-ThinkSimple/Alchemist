340b8d94cf1255dac21e780184037ab324118cef
batuogkoc <bgunduz21@ku.edu.tr>
2024-01-18 22:57:41 +0300

    Furthered the host adapter impl.

--- domain/initialization/OnlineHostGameInitializerAdapter.java
+++ domain/initialization/OnlineHostGameInitializerAdapter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Map;
 
 import domain.LocalData;
+import network.Client;
 import network.Server;
 
 public class OnlineHostGameInitializerAdapter implements GameInitializerAdapter{
@@ -18,9 +19,12 @@ public class OnlineHostGameInitializerAdapter implements GameInitializerAdapter{
 			
 			server = new Server(port, bufferSize);
 			server.start();
+			
+			client = new Client("host", "127.0.0.1", port);
+			client.start();
 		}
 		catch(IOException ioe) {
-			throw new Exception("Could not start server");
+			throw new Exception("Could not start server, or connect to local server.");
 		}
 		catch (Exception e) {
 			throw new Exception("Invalid arguments");
@@ -30,7 +34,7 @@ public class OnlineHostGameInitializerAdapter implements GameInitializerAdapter{
 		new InitializeGameHelper();	
 		//TODO: how to set local player?
 		//LocalData.getInstance().setLocalPlayer(null);
-		server.
+		server
 	}
 
 }
